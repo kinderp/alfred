28dab71ba4cf3da8e01de534e12b12838392dfa3
kinderp <a.caristia@gmail.com>
2026-05-15 20:15:33 +0000

    Rollback main.c

--- src/main.c
+++ src/main.c
@@ -7,16 +7,9 @@ int main(int argc, char **argv)
     app_t app;
 
     int rc = app_init(&app, argc, argv);
-
-    if (rc != 0) {
-
-        fprintf(stderr,
-                "startup failed\n");
-	/* free memory if init failed  but this can 
-	 * can be done only if app_shutdown is safe
-	 * even with partial app_init */
-        app_shutdown(&app);
-
+    
+    if (app_init(&app, argc, argv) != 0) {
+        fprintf(stderr, "startup failed\n");
         return 1;
     }
 
